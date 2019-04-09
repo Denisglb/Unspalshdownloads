1a18e33d19bc477739dbafc0cc4c8d7d616e78ba
Denis <denisglb@yahoo.co.uk>
2019-04-10 00:02:46 +0100

    added gitignore file and added a timeout if request is used over 45 times

---
+++ exists.rb
@@ -0,0 +1,5 @@
+if(File.file?('hello.rb')) 
+    puts 'file exists'
+else 
+    puts 'file not found'
+end
--- main.rb
+++ main.rb
@@ -2,39 +2,50 @@ require 'rubygems'
 require 'httparty'
 require 'open-uri'
 require 'json'
+require 'active_support/time'
+require "yaml"
 
 class Unsplashed
 	include HTTParty
 	attr_reader :options
 
 	base_uri 'https://api.unsplash.com'
-		
+
 	def initialize
-    	@api_key  = "4f64410d0f89accae239cbb1fa2adaf4d0e8fd995d4349fc3136ae9fb9a027f9"
+		@apikey = YAML::load_file('config.yml')["apikey"]
     	@collection =[]
     	@page_count = 0
+    	@total = 0
   	end
 
 	def page_count(query)
-		data = self.class.get('/search/photos', options(query))
+		data = self.class.get('/search/photos', options(query, 1))
 		if (data.headers["x-total"].to_i / 30) == 0
 			@page_count =1
 		else
 			@page_count = (data.headers["x-total"].to_i / 30)
-			p @page_count
 		end
+		p "Number of pages = " + (@page_count).to_s
+		p "Number of photos to download = " + data.headers["x-total"]
+		@total = data.headers["x-total"].to_i
+		p "Number of hours = " + (@page_count/45).to_s
 	end
 
 	def controller(query)
 		page_count(query)
-		2.to_i.times do |i|
+		Dir.mkdir "./#{query}"
+		for i in (1..@page_count.to_i)
+			if i%45 == 0
+				sleep(1.hours)
+			end
 			get_data(query, i)
+			download_images(query, i)
+			@collection = []
 		end
-		download_images(query)
 	end
 
 	def get_data(query, page)
-		data = self.class.get('/search/photos', options(query))
+		data = self.class.get('/search/photos', options(query, page))
 		body = JSON.parse(data.body)
 		download_url = body["results"]
 		download_url.each {|hash| 
@@ -42,21 +53,20 @@ class Unsplashed
 		}
 	end
 
-	def options(query, page=1)
+	def options(query, page)
 		{query: {
-	      	page: page.to_s,
+	      	page: page,
 	        per_page: "30",
 	        query: query,
-	        client_id: @api_key
+	        client_id: @apikey
       		}
     	}
 	end
 
-	def download_images(query)
-		Dir.mkdir "./#{query}"
+	def download_images(query, number)
 		@collection.each_with_index { |down, index|
   		open("#{down}") {|f|
-   		File.open("./#{query}/Image #{index + 1} of #{query}.jpg","wb") do |file|
+   		File.open("./#{query}/Image #{index+1} from page #{number} of #{query}.jpg","wb")  do |file|
      	file.puts f.read end
      	}
      }
